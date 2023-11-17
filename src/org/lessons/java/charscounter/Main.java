d39b5f969787b8f407e11739af77b89e6da960a2
Flopiras <Flopiras@yahoo.it>
2023-11-17 14:38:51 +0100

    added map and characters counter

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,9 +1,36 @@
 package org.lessons.java.charscounter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Inserisci una parola: ");
+		String word = in.nextLine();
+
+		in.close();
+
+		Map<Character, Integer> charFreq = new HashMap<>();
+
+		for (Character c : word.toLowerCase().toCharArray()) {
+
+			if (charFreq.containsKey(c)) {
+				charFreq.put(c, charFreq.get(c) + 1);
+			} else {
+				charFreq.put(c, 1);
+			}
+		}
+
+		for (Character key : charFreq.keySet()) {
+
+			int value2 = charFreq.get(key);
+			System.out.println(key + " : " + value2);
+		}
 
 	}
 
